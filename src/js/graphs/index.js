4b5b6d2265b2ef81562aedc1f424b93f9eea600c
Alexey Karpov <karpovad@yandex.ru>
2015-08-30 08:11:41 +0300

    Стиль кода Добавил закраску графика справа.

--- src/js/graphs/index.js
+++ src/js/graphs/index.js
@@ -25,7 +25,7 @@ let graphs = (leftID, rightID) => {
   let plotterLeft = new Plotter(leftID, plotterOptions);
   let plotterRight = new Plotter(rightID, plotterOptions);
 
-  let func = x => x*x;
+  let func = x => x * x;
 
   let point = plotterLeft.addPoint(GraphConstants.getMean(), func(GraphConstants.getMean()), {
     color: '#ff0000',
@@ -45,10 +45,15 @@ let graphs = (leftID, rightID) => {
     let [x1, y1, x2, y2] = [borders.left, func(borders.left), borders.right, func(borders.right)];
     lineLeft = dashedLine(plotterLeft, x1, y1, x2, y2);
     let linearFunction = (x) => (y2 - y1) / (x2 - x1) * x + y1 - (y2 - y1) / (x2 - x1) * x1;
-    lineRight = plotterRight.addFunc((x) => Math.abs(linearFunction(x) - func(x)), {
-      left: x1,
-      right: x2
-    });
+    lineRight = [
+      plotterRight.addShadedArea((x) => Math.abs(linearFunction(x) - func(x)), {
+        left: x1,
+        right: x2
+      }), plotterRight.addFunc((x) => Math.abs(linearFunction(x) - func(x)), {
+        left: x1,
+        right: x2
+      })
+    ];
   };
   let updateDashedLine = () => {
     if (!isDashedLineDrawn) {
@@ -61,55 +66,59 @@ let graphs = (leftID, rightID) => {
     lineLeft.Y1(y1);
     lineLeft.Y2(y2);
     let linearFunction = (x) => (y2 - y1) / (x2 - x1) * x + y1 - (y2 - y1) / (x2 - x1) * x1;
-    lineRight.Func((x) => Math.abs(linearFunction(x) - func(x)));
-    lineRight.Left(x1);
-    lineRight.Right(x2);
+    for (let element of lineRight) {
+      element.Func((x) => Math.abs(linearFunction(x) - func(x)));
+      element.Left(x1);
+      element.Right(x2);
+    }
     plotterRight.redraw();
     plotterLeft.redraw();
   };
   let removeDashedLine = () => {
     isDashedLineDrawn = false;
     plotterLeft.remove(lineLeft);
-    plotterRight.remove(lineRight);
+    for (let element of lineRight) {
+      plotterRight.remove(element);
+    }
   };
   if (CheckboxesStore.isChecked(CheckboxesStore.names.EnableDashedLine)) {
     drawDashedLine();
   }
   Dispatcher.register((event) => {
-      switch(event.actionType) {
-        case SliderConstants.SLIDER_SLIDE:
-          if (event.name === SlidersStore.names.pointPosition) {
-            point.X(event.value);
-            point.Y(func(event.value));
+    switch (event.actionType) {
+      case SliderConstants.SLIDER_SLIDE:
+        if (event.name === SlidersStore.names.pointPosition) {
+          point.X(event.value);
+          point.Y(func(event.value));
 
-            if (CheckboxesStore.isChecked(CheckboxesStore.names.EnableDashedLine)) {
-              updateDashedLine();
-            } else {
-              plotterLeft.redraw();
-            }
+          if (CheckboxesStore.isChecked(CheckboxesStore.names.EnableDashedLine)) {
+            updateDashedLine();
+          } else {
+            plotterLeft.redraw();
           }
-          break;
+        }
+        break;
 
-        case CheckboxConstants.CHECKBOX_TOGGLE:
-          if (event.name === CheckboxesStore.names.EnableDashedLine) {
-            CheckboxesStore.isChecked(CheckboxesStore.names.EnableDashedLine) ?
-              drawDashedLine() :
-              removeDashedLine();
-          }
+      case CheckboxConstants.CHECKBOX_TOGGLE:
+        if (event.name === CheckboxesStore.names.EnableDashedLine) {
+          CheckboxesStore.isChecked(CheckboxesStore.names.EnableDashedLine) ?
+            drawDashedLine() :
+            removeDashedLine();
+        }
 
-          if (event.name == CheckboxesStore.names.ZoomGraphs) {
-            zoom.center = {
-              x: parseFloat(point.X()),
-              y: parseFloat(point.Y())
-            };
-            CheckboxesStore.isChecked(CheckboxesStore.names.ZoomGraphs) ?
-              (zoom.zoomIn(),
+        if (event.name == CheckboxesStore.names.ZoomGraphs) {
+          zoom.center = {
+            x: parseFloat(point.X()),
+            y: parseFloat(point.Y())
+          };
+          CheckboxesStore.isChecked(CheckboxesStore.names.ZoomGraphs) ?
+            (zoom.zoomIn(),
               updateDashedLine()) :
-              (zoom.zoomOut(),
+            (zoom.zoomOut(),
               updateDashedLine());
-          }
-          break;
-      }
+        }
+        break;
+    }
   });
 };
 
